9a09522904d7f00e26d6d68c98868f00c475bdd8
Marcel Licence <marcel.licence@gmx.de>
2022-01-29 11:25:54 +0100

    Add missing configuration for I2S which can be found on the ESP32 Audio Kit

--- src/boards/board_audio_kit_es8388.h
+++ src/boards/board_audio_kit_es8388.h
@@ -1,5 +1,31 @@
 /*
  * Copyright (c) 2021 Marcel Licence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ * veröffentlichten Version, weiter verteilen und/oder modifizieren.
+ *
+ * Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
+ * OHNE JEDE GEWÄHR,; sogar ohne die implizite
+ * Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Einzelheiten.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
  */
 
 /**
@@ -71,6 +97,13 @@
 #define ES8388_PIN_DIN  26
 #define ES8388_PIN_LRCK 25
 #define ES8388_PIN_SCLK 5
+#define ES8388_PIN_MCLK 0
+#elif ES8388_CFG_I2S==5
+/* please refer to lyrat_v4_3 pins configuration which can be found on the ESP32 Audio Kit as-well */
+#define ES8388_PIN_DOUT 35
+#define ES8388_PIN_DIN  26
+#define ES8388_PIN_LRCK 25
+#define ES8388_PIN_SCLK 27
 #define ES8388_PIN_MCLK 0
 #endif
 
