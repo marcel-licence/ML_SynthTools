e11a6efd510056b360451b46f6537b28e193e06e
Marcel Licence <marcel.licence@gmx.de>
2023-04-21 19:38:52 +0200

    Add missing adc header

---
+++ src/rp2040_adc.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 Marcel Licence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ * veröffentlichten Version, weiter verteilen und/oder modifizieren.
+ *
+ * Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
+ * OHNE JEDE GEWÄHR,; sogar ohne die implizite
+ * Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Einzelheiten.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file rp2040_adc.h
+ * @author Marcel Licence
+ * @date 08.04.2023
+ *
+ * @brief   This module allows to read adc value using interrupts and DMA
+ */
+
+
+#ifndef RP2040_ADC_H_
+#define RP2040_ADC_H_
+
+
+void adc_setup(void *cap_Buf, uint32_t depth);
+bool adc_done();
+void restartAdc();
+void adc_stop();
+
+
+#endif /* RP2040_ADC_H_ */
