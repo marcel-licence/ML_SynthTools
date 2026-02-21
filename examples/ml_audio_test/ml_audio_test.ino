efbd228848b8ee39b987e9b8c0d269ac5d5d72f6
Marcel Licence <marcel.licence@gmx.de>
2026-02-21 13:37:53 +0100

    Fix compilation errors

--- examples/ml_audio_test/ml_audio_test.ino
+++ examples/ml_audio_test/ml_audio_test.ino
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Marcel Licence
+ * Copyright (c) 2026 Marcel Licence
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,15 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Dieses Programm ist Freie Software: Sie k�nnen es unter den Bedingungen
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
  * der GNU General Public License, wie von der Free Software Foundation,
  * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
- * ver�ffentlichten Version, weiter verteilen und/oder modifizieren.
+ * veröffentlichten Version, weiter verteilen und/oder modifizieren.
  *
- * Dieses Programm wird in der Hoffnung bereitgestellt, dass es n�tzlich sein wird, jedoch
- * OHNE JEDE GEW�HR,; sogar ohne die implizite
- * Gew�hr der MARKTF�HIGKEIT oder EIGNUNG F�R EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License f�r weitere Einzelheiten.
+ * Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
+ * OHNE JEDE GEWÄHR,; sogar ohne die implizite
+ * Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Einzelheiten.
  *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
@@ -38,15 +38,25 @@
  */
 #define OUTPUT_SAW_TEST
 
+/*
+ * The following configuration is for the RP2040, RP2350
+ */
+#ifdef ARDUINO_ARCH_RP2040
+
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+
+
+#endif
 
 /*
- * Configuration for
- * Board: "ESP32 Dev Module" or similar
+ * the following configuration is for ESP32, ESP32S2, ..
  */
 #ifdef ESP32
 
 //#define BOARD_ML_V1 /* activate this when using the ML PCB V1 */
-#define BOARD_ML_SYNTH_V2 /* activate this when using the ML PCB V1 */
+//#define BOARD_ML_SYNTH_V2 /* activate this when using the ML PCB V1 */
 //#define BOARD_ESP32_AUDIO_KIT_AC101 /* activate this when using the ESP32 Audio Kit v2.2 with the AC101 codec */
 //#define BOARD_ESP32_AUDIO_KIT_ES8388 /* activate this when using the ESP32 Audio Kit v2.2 with the ES8388 codec */
 //#define BOARD_ESP32_DOIT /* activate this when using the DOIT ESP32 DEVKIT V1 board */
@@ -56,13 +66,66 @@
  * include the board configuration
  * there you will find the most hardware depending pin settings
  */
-#include <ml_boards.h> /* requires the ML_Synth library:  https://github.com/marcel-licence/ML_SynthTools */
+//#include <ml_boards.h> /* requires the ML_Synth library:  https://github.com/marcel-licence/ML_SynthTools */
 
 #define SAMPLE_RATE 44100
 #define SAMPLE_SIZE_16BIT
 #define SAMPLE_BUFFER_SIZE  48
 
 #endif /* ESP32 */
+
+
+/*
+ * the following configuration is for Teensy
+ */
+#ifdef TEENSYDUINO // CORE_TEENSY
+
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+
+#endif
+
+/*
+ * Configuration for
+ * Board: "LOLIN(WEMOS) D1 R2 & mini 2 or similar
+ */
+#ifdef ESP8266
+
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+#define AUDIO_OUT_MONO
+
+#endif
+
+
+/*
+ * Configuration for
+ * Board: "Generic STM32H7 Series"
+ * Board part number: "Daisy Seed"
+ */
+#ifdef ARDUINO_DAISY_SEED
+
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+
+#endif /* ARDUINO_DAISY_SEED */
+
+
+/*
+ * Configuration for
+ * Board: "Seeeduino XIAO"
+ */
+#if (defined ARDUINO_SEEED_XIAO_M0) || (defined SEEED_XIAO_M0)
+
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE_16BIT
+#define SAMPLE_BUFFER_SIZE  48
+#define AUDIO_OUT_MONO
+
+#endif /* (defined ARDUINO_SEEED_XIAO_M0) || (defined SEEED_XIAO_M0) */
 
 
 #define ML_SYNTH_INLINE_DECLARATION
@@ -86,9 +149,17 @@ void setup()
 
 void loop()
 {
+#ifndef AUDIO_OUT_MONO
     float left[SAMPLE_BUFFER_SIZE], right[SAMPLE_BUFFER_SIZE];
     /*
      * Output the audio
      */
     Audio_Output(left, right);
+#else
+    int32_t mono[SAMPLE_BUFFER_SIZE];
+    /*
+     * Output the audio
+     */
+    Audio_OutputMono(mono);
+#endif
 }
